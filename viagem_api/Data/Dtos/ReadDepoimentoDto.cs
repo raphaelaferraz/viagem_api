3789617dee69b2ed6bebcaed0ecf665de0a91925
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 08:49:21 -0300

    Criação da classe Dto de leitura de Depoimento

---
+++ viagem_api/Data/Dtos/ReadDepoimentoDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace viagem_api.Data.Dtos;
+
+public class ReadDepoimentoDto
+{
+    public string UrlFoto { get; }
+    public string NomeUsuario { get; }
+    public string DepoimentoUsuario { get; }
+
+    public ReadDepoimentoDto(string urlFoto, string nomeUsuario, string depoimentoUsuario)
+    {
+        UrlFoto = urlFoto;
+        NomeUsuario = nomeUsuario;
+        DepoimentoUsuario = depoimentoUsuario;
+    }
+}
