9ecbce8a4ccb47fdaae97ef6649fb6b7cc5f644f
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 09:12:55 -0300

    Adição do controller de Depoimento e configuração de dependências do AutoMapper

---
+++ viagem_api/Controllers/DepoimentoController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using viagem_api.Data.Dtos;
+using viagem_api.Models;
+
+namespace viagem_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DepoimentoController : ControllerBase
+{
+    private List<dynamic> _data;
+    private IMapper _mapper;
+
+    public DepoimentoController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public List<dynamic> AdicionaDepoimento(CreateDepoimentoDto depoimentoDto)
+    {
+        var depoimento = _mapper.Map<Depoimento>(depoimentoDto);
+
+        _data.Add(depoimento);
+
+        return _data;
+    }
+}
--- viagem_api/Program.cs
+++ viagem_api/Program.cs
@@ -2,10 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
