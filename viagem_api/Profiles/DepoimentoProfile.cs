8905a684c6f9b8f90346a84abf6f05b294813b3f
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 08:54:50 -0300

    Criação da classe Profile para os Dtos de Depoimento

---
+++ viagem_api/Profiles/DepoimentoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using viagem_api.Data.Dtos;
+using viagem_api.Models;
+
+namespace viagem_api.Profiles;
+
+public class DepoimentoProfile : Profile
+{
+    public DepoimentoProfile() 
+    {
+        CreateMap<CreateDepoimentoDto, Depoimento>();
+        CreateMap<Depoimento, ReadDepoimentoDto>();
+        CreateMap<UpdateDepoimentoDto, Depoimento>();
+    }
+}
