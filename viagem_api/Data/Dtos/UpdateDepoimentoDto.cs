db063e9dd05c228f5950caeb145d13eac80af463
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 08:50:14 -0300

    Criação da classe Dto de atualização de Depoimento

---
+++ viagem_api/Data/Dtos/UpdateDepoimentoDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace viagem_api.Data.Dtos;
+
+public class UpdateDepoimentoDto
+{
+    [Required(ErrorMessage = "A foto é obrigatória.")]
+    public string UrlFoto { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [StringLength(50, ErrorMessage = "O nome deve ter apenas 50 caracteres.")]
+    public string NomeUsuario { get; set; }
+
+    [Required(ErrorMessage = "O depoimento é obrigatório.")]
+    [StringLength(255, ErrorMessage = "O depoimento deve ter apenas 250 caracteres.")]
+    public string DepoimentoUsuario { get; set; }
+}
