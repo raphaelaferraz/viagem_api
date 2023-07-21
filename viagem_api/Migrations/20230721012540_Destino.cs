72f12ae4c216cfd8e56d307cd5e254236f472395
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 22:26:35 -0300

    Adição da migração com a nova tabela no banco de dados

---
+++ viagem_api/Migrations/20230721012540_Destino.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace viagem_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Destino : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Destino",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Preco = table.Column<decimal>(type: "numeric", nullable: false),
+                    UrlFoto = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destino", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destino");
+        }
+    }
+}
