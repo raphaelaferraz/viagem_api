f2ef21a3c046cd1f1e1f52bb6677d991ee3a182d
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 22:19:10 -0300

    Criação da classe Dto de atualização de Destino

---
+++ viagem_api/Data/Dtos/UpdateDestinoDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace viagem_api.Data.Dtos;
+
+public class UpdateDestinoDto
+{
+    [Required(ErrorMessage = "O nome do destino é obrigatório.")]
+    [StringLength(50, ErrorMessage = "O nome do destino deve ter apenas 50 caracteres.")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O preço do destino é obrigatório.")]
+    public decimal Preco { get; set; }
+}
