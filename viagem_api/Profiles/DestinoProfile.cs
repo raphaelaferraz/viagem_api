4bca7afa2a674fe887d638e862a6a82aa24f031d
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 22:23:08 -0300

    Criação da classe profile de Destino

---
+++ viagem_api/Profiles/DestinoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using viagem_api.Data.Dtos;
+using viagem_api.Models;
+
+namespace viagem_api.Profiles;
+
+public class DestinoProfile : Profile
+{
+    public DestinoProfile()
+    {
+        CreateMap<CreateDestinoDto, Destino>();
+        CreateMap<Destino, ReadDestinoDto>();
+        CreateMap<UpdateDestinoDto, Destino>();
+    }
+}
