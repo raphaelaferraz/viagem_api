d747c6a2167425edc50a7aec4c90ffa44db30d01
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 22:16:35 -0300

    Criação da classe model Destino

---
+++ viagem_api/Models/Destino.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace viagem_api.Models;
+
+public class Destino
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome do destino é obrigatório.")]
+    [StringLength(50, ErrorMessage = "O nome do destino deve ter apenas 50 caracteres.")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O preço do destino é obrigatório.")]
+    public decimal Preco { get; set; }
+}
