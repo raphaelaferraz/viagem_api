bc1e2935cacc75e40439819b4eeaf0ee288b0e11
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-20 22:18:32 -0300

    Criação da classe Dto de leitura do Destino

---
+++ viagem_api/Data/Dtos/ReadDestinoDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace viagem_api.Data.Dtos;
+
+public class ReadDestinoDto
+{
+    public string Nome { get; set; }
+
+    public decimal Preco { get; set; }
+}
