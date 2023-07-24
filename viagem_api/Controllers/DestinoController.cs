c5ad39166c15e04195e544a0dd9de12c8dbe5d17
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-07-24 17:51:07 -0300

    Adição do método Post do controller de Destino

---
+++ viagem_api/Controllers/DestinoController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using viagem_api.Data;
+using viagem_api.Data.Dtos;
+using viagem_api.Migrations;
+using viagem_api.Models;
+
+namespace viagem_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DestinoController : ControllerBase
+{
+    private ViagemContext _context;
+    private IMapper _mapper;
+
+    public DestinoController(ViagemContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult AdicionaDestino(CreateDestinoDto destinoDto)
+    {
+        var destino = _mapper.Map<viagem_api.Models.Destino>(destinoDto);
+
+        _context.Destino.Add(destino);
+
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(ListaDestinos), new { id = destino.Id }, destino);
+    }
+
+}
